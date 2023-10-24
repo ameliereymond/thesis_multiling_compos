2c631f194ab480a6c3bb5b8cf0c17c88ee595631
Amélie T. Reymond <amelietamrey@gmail.com>
2023-10-23 18:16:25 -0700

    Add LLAMA test

--- condor-run.sh
+++ condor-run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 conda init bash
-conda activate mscan
+conda activate mscan2
 
-./scripts/run_xglm_inference.sh > run.out
+export CUDA_VISIBLE_DEVICES="0,1"
+python src/gpu_test.py > run-gpu.out
+
+python ./src/experiment_llama.py \
+    --train data/output/en/simple/train.txt \
+    --test data/output/en/simple/test.txt \
+    --output data/output/results/llama/en/simple/results.json \
+     > run-llama-en-simple.out
--- condor-task.cmd
+++ condor-task.cmd
@@ -3,6 +3,7 @@ getenv     = true
 error      = run.error
 log        = run.log
 notification = complete
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
 transfer_executable = false
 request_GPUs = 1
 queue
