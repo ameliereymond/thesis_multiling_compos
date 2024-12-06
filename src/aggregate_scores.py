6ff40329de8b2d7879ad40d51343a2163083ff4d
Amelie Reymond <amelietamrey@gmail.com>
2024-12-05 22:09:03 -0800

    Fix score aggregation for strategies and versions

--- src/aggregate_scores.py
+++ src/aggregate_scores.py
@@ -2,29 +2,34 @@ import os
 from pathlib import Path
 import json
 import csv
+from common import MODELS, LANGUAGES, SPLITS, STRATEGIES, VERSIONS
 
 models = ["aya", "bloomz", "bloomz-mt", "bloom", "xglm", "bloom"] # "llama-3-8B", "llama-3-8B-instruct"]
 splits = ["mcd1", "mcd2", "mcd3", "add_prim_jump", "add_prim_turn_left", "length_split", "simple"]
 langs = ["en", "fr", "cmn", "hin", "ru"]
 
 def get_scores():
-    for model in models:
-        for split in splits:
-            for lang in langs:
-                results_path = Path("data") / "output" / "results" / model / lang / split / "score.json"
-                with open(results_path, 'r') as f:
-                    data = json.load(f)
-
-                yield {
-                    "model": model,
-                    "lang": lang,
-                    "split": split,
-                    "number_samples": data["number_samples"],
-                    "sum_exact_matches": data["sum_exact_matches"],
-                    "sum_exact_prefixes": data["sum_exact_prefixes"],
-                    "avg_edit_distance": data["avg_edit_distance"],
-                    "avg_expected_length": data["avg_expected_length"]
-                }
+    for model in MODELS.keys():
+        for lang in LANGUAGES:
+            for split in SPLITS:
+                for strategy in STRATEGIES:
+                    for version in VERSIONS.keys():
+                        results_path = Path("data") / "output" / "results" / model / lang / split / strategy / version / "score.json"
+                        if results_path.exists():
+                            with open(results_path, 'r') as f:
+                                data = json.load(f)
+                                yield {
+                                    "model": model,
+                                    "lang": lang,
+                                    "split": split,
+                                    "number_samples": data["number_samples"],
+                                    "sum_exact_matches": data["sum_exact_matches"],
+                                    "sum_exact_prefixes": data["sum_exact_prefixes"],
+                                    "avg_edit_distance": data["avg_edit_distance"],
+                                    "avg_expected_length": data["avg_expected_length"]
+                                }
+                        else:
+                            print(f"Skipping {results_path} as it does not exist")
 
 def write_aggregated_results(output_file: Path):
     with open(output_file, "w") as f:
